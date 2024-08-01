d3cd3107a63e388189e152606d00b3c4b57735a2
roodra <roodra.parihar@deeplogictech.com>
2024-08-01 10:49:28 +0530

    user model schema

---
+++ models/userModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+
+// schema
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Name is required"]
+    },
+
+    lastname:{
+        type:String
+    },
+
+    email:{
+        type:String,
+        required:[true,"email is required"],
+        unique: true,
+        validate: validator.isEmail 
+    },
+
+    password:{
+        type: String,
+        required:[true,"password is required"]
+
+    },
+
+    location:{
+        type:String,
+        default:"India"
+    }
+
+},{timestamps:true});
+
+export default mongoose.model("User",userSchema);
